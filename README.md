97e4c67b52cb9fe80b011701dfb8b32eabb86526
Matthieu Leboeuf <contact@matthieul.dev>
2020-08-21 21:44:55 +0200

    add files

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# bbcollab-bot
+# Bot bbcollab
+Bot bbcollab pour se connecter au visios conférences sans se réveiller !
+
+## Comment ça marche ?
+
+Bbcollab ne disposant pas d'API permettant directement de se connecter, le bot utilise Selenium, une librarie. Pour chaque nom mis dans le fichier de configuration, le bot va ouvrir un nouveau navigateur, se rendre sur l'adresse spécifiée dans la configuration.
+
+## Installer le bot
+
+Pour installer le bot, ouvrez le terminal et tapez les commandes suivantes :
+
+il faut télécharger le driver pour firefox : [ici](https://github.com/mozilla/geckodriver/releases) et l'ajouter dans votre PATH
+
+* `git clone https://github.com/MatthieuLeboeuf/bbcollab-bot.git` - Téléchargement des fichiers
+* `npm install --production` - Installation des dépendances du bot
+* `node index.js` - Démarrage du bot
+
+### Fichier de configuration
+
+Pour que le bot fonctionne correctement, vous devez remplir le fichier de configuration. Copiez le fichier `config.sample.json` en un nouveau fichier `config.json`. Puis, éditez le avec les valeurs suivantes :
+
+* `url`: l'adresse de connexion bbcollab
+* `time`: l'heure à laquelle le bot doit se connecter
+* `time_bypass`: bypass le temps et lancer le bot immédiatement
+* `users`: tout les noms
---
+++ index.js
@@ -0,0 +1,66 @@
+const {Builder, By, until} = require('selenium-webdriver');
+const moment = require('moment');
+const config = require('./config.json');
+const firefox = require('selenium-webdriver/firefox');
+
+const join = Date.parse(moment(config.time, "DD-MM-YYYY HH:mm:ss"));
+const to = join-Date.now();
+
+let i = 0;
+
+if(config.time_bypass == "true"){
+    console.log("Démarrage en mode bypass");
+    startBrowser();
+} else {
+    if(to < 0){
+        console.log("L'heure indiqué est incorrecte !");
+        return false;
+    }
+    if(config.users.length == 1){
+        console.log("La session sera lancé dans " + moment(to).format('mm,ss') + " minutes");
+    } else if(config.users.length > 1) {
+        console.log("Les sessions seront lancés dans " + moment(to).format('mm,ss') + " minutes");
+    }
+    setTimeout(startBrowser, to);
+}
+function startBrowser() {
+    const options = new firefox.Options();
+    options.setPreference("permissions.default.microphone", 1);
+    options.setPreference("permissions.default.camera", 2);
+    (async function firefox() {
+        let driver = await new Builder().forBrowser('firefox').setFirefoxOptions(options).build();
+            await driver.get(config.url);
+
+            await driver.wait(until.elementLocated(By.name('guestnameform')), 60000);
+            var name = await driver.findElement(By.name('guestnameform'));
+            var input = await name.findElement(By.id('guest-name'));
+            input.sendKeys(config.users[i]);
+
+            await driver.wait(until.elementLocated(By.id('launch-html-guest')), 60000);
+            await driver.manage().setTimeouts( { implicit: 1000 } );
+            var btn = await driver.findElement(By.id('launch-html-guest'));
+            btn.click();
+
+            //micro
+            await driver.wait(until.elementLocated(By.className('button confirm button--inverse ng-scope')), 60000);
+            await driver.manage().setTimeouts( { implicit: 1000 } );
+            var mic = await driver.findElement(By.className('button confirm button--inverse ng-scope'));
+            mic.click();
+            //video
+            await driver.wait(until.elementLocated(By.id('techcheck-video-ok-button')), 60000);
+            var video = await driver.findElement(By.id('techcheck-video-ok-button'));
+            video.click();
+
+            //info
+            await driver.wait(until.elementLocated(By.id('announcement-modal-page-wrap')), 60000);
+            var info = await driver.findElement(By.id('announcement-modal-page-wrap'));
+            var info2 = await info.findElement(By.className('close'));
+            info2.click();
+
+            if(i < config.users.length-1){
+                i++;
+                startBrowser();
+            }
+    })().catch( e => { console.error(e) } );
+}
+  
